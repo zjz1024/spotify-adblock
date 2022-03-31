3f83bfb0d7e574da1df672f0535797bbd755a2d1
zjz1024 <96932345+zjz1024@users.noreply.github.com>
2022-03-31 17:43:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+import os
+import subprocess
+import time
+import pyautogui
+
+print("Welcome to zjz's spotify skip. You may need to restart this exe after a while of inactivity on spotify as your\ntoken will no longer work")
+print("You will need to get your token from https://developer.spotify.com/console/get-users-currently-playing-track/ \nPress get token and tick everything in required scopes(there should only be one) and press request token.\nYour token will be generated in the box that writes OAuth token. Copy the token and prepare to paste it")
+path = input("Enter the path of spotify.exe(It should look like : 'C:/Users/<yourname>/AppData/Roaming/Spotify/Spotify.exe'): ")  # Locate the path of spotify.exe on your computer, It should look like : 'C:/Users/<yourname>/AppData/Roaming/Spotify/Spotify.exe'
+SPOTIFY_ACCESS_TOKEN = input("Enter token: ") #enter your spotify token
+print(SPOTIFY_ACCESS_TOKEN)
+print(path)
+# ----------------------------------------
+adCount = 0
+SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+
+def get_current_track(access_token):
+    response = requests.get(
+        SPOTIFY_GET_CURRENT_TRACK_URL,
+        headers={
+            "Authorization": f'Bearer {access_token}'
+        }
+    )
+
+    resp_json = response.json()
+
+    play_type = resp_json['currently_playing_type']
+
+    if play_type == 'ad':
+        IS_AD = True
+    else:
+        IS_AD = False
+
+    return IS_AD
+
+
+def close_spotify():
+    os.system(f'taskkill /F /IM Spotify.exe')
+
+
+def reopen_spotify():
+    subprocess.Popen(path)
+
+
+def main():
+    IS_AD = current_track = get_current_track(
+        SPOTIFY_ACCESS_TOKEN
+    )
+    print("Valid")
+    if IS_AD:
+        print("Found an ad, restarting Spotify")
+        close_spotify()
+        reopen_spotify()
+        time.sleep(2)
+        pyautogui.keyDown('playpause')
+        time.sleep(0.5)
+        pyautogui.keyUp('playpause')
+
+
+
+
+if __name__ == '__main__':
+   while True:
+
+        try:
+            main()
+        except:
+            print('Invalid')
